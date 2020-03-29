984ea7cdb22be441efdbccf848f48fa216f4d654
Alexey Lis <30868047+renardein@users.noreply.github.com>
2020-03-29 13:40:34 +0700

    Release

---
+++ Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace Two_Dimensional_Array_Sorter2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.a11 = new System.Windows.Forms.TextBox();
+            this.a12 = new System.Windows.Forms.TextBox();
+            this.a13 = new System.Windows.Forms.TextBox();
+            this.a14 = new System.Windows.Forms.TextBox();
+            this.a15 = new System.Windows.Forms.TextBox();
+            this.a21 = new System.Windows.Forms.TextBox();
+            this.a31 = new System.Windows.Forms.TextBox();
+            this.a41 = new System.Windows.Forms.TextBox();
+            this.a51 = new System.Windows.Forms.TextBox();
+            this.a22 = new System.Windows.Forms.TextBox();
+            this.a23 = new System.Windows.Forms.TextBox();
+            this.a24 = new System.Windows.Forms.TextBox();
+            this.a25 = new System.Windows.Forms.TextBox();
+            this.a32 = new System.Windows.Forms.TextBox();
+            this.a33 = new System.Windows.Forms.TextBox();
+            this.a34 = new System.Windows.Forms.TextBox();
+            this.a35 = new System.Windows.Forms.TextBox();
+            this.a43 = new System.Windows.Forms.TextBox();
+            this.a42 = new System.Windows.Forms.TextBox();
+            this.a45 = new System.Windows.Forms.TextBox();
+            this.a44 = new System.Windows.Forms.TextBox();
+            this.a55 = new System.Windows.Forms.TextBox();
+            this.a54 = new System.Windows.Forms.TextBox();
+            this.a53 = new System.Windows.Forms.TextBox();
+            this.a52 = new System.Windows.Forms.TextBox();
+            this.doneMatrix = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.doneMatrix.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.a55);
+            this.groupBox1.Controls.Add(this.a54);
+            this.groupBox1.Controls.Add(this.a53);
+            this.groupBox1.Controls.Add(this.a52);
+            this.groupBox1.Controls.Add(this.a44);
+            this.groupBox1.Controls.Add(this.a45);
+            this.groupBox1.Controls.Add(this.a42);
+            this.groupBox1.Controls.Add(this.a43);
+            this.groupBox1.Controls.Add(this.a35);
+            this.groupBox1.Controls.Add(this.a34);
+            this.groupBox1.Controls.Add(this.a33);
+            this.groupBox1.Controls.Add(this.a32);
+            this.groupBox1.Controls.Add(this.a25);
+            this.groupBox1.Controls.Add(this.a24);
+            this.groupBox1.Controls.Add(this.a23);
+            this.groupBox1.Controls.Add(this.a22);
+            this.groupBox1.Controls.Add(this.a51);
+            this.groupBox1.Controls.Add(this.a41);
+            this.groupBox1.Controls.Add(this.a31);
+            this.groupBox1.Controls.Add(this.a21);
+            this.groupBox1.Controls.Add(this.a15);
+            this.groupBox1.Controls.Add(this.a14);
+            this.groupBox1.Controls.Add(this.a13);
+            this.groupBox1.Controls.Add(this.a12);
+            this.groupBox1.Controls.Add(this.a11);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 150);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введите квадратную матрицу ";
+            // 
+            // a11
+            // 
+            this.a11.Location = new System.Drawing.Point(7, 20);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(27, 20);
+            this.a11.TabIndex = 0;
+            // 
+            // a12
+            // 
+            this.a12.Location = new System.Drawing.Point(40, 20);
+            this.a12.Name = "a12";
+            this.a12.Size = new System.Drawing.Size(27, 20);
+            this.a12.TabIndex = 1;
+            // 
+            // a13
+            // 
+            this.a13.Location = new System.Drawing.Point(73, 20);
+            this.a13.Name = "a13";
+            this.a13.Size = new System.Drawing.Size(27, 20);
+            this.a13.TabIndex = 2;
+            // 
+            // a14
+            // 
+            this.a14.Location = new System.Drawing.Point(106, 20);
+            this.a14.Name = "a14";
+            this.a14.Size = new System.Drawing.Size(27, 20);
+            this.a14.TabIndex = 3;
+            // 
+            // a15
+            // 
+            this.a15.Location = new System.Drawing.Point(139, 20);
+            this.a15.Name = "a15";
+            this.a15.Size = new System.Drawing.Size(27, 20);
+            this.a15.TabIndex = 4;
+            // 
+            // a21
+            // 
+            this.a21.Location = new System.Drawing.Point(7, 46);
+            this.a21.Name = "a21";
+            this.a21.Size = new System.Drawing.Size(27, 20);
+            this.a21.TabIndex = 8;
+            // 
+            // a31
+            // 
+            this.a31.Location = new System.Drawing.Point(7, 72);
+            this.a31.Name = "a31";
+            this.a31.Size = new System.Drawing.Size(27, 20);
+            this.a31.TabIndex = 9;
+            // 
+            // a41
+            // 
+            this.a41.Location = new System.Drawing.Point(7, 98);
+            this.a41.Name = "a41";
+            this.a41.Size = new System.Drawing.Size(27, 20);
+            this.a41.TabIndex = 10;
+            // 
+            // a51
+            // 
+            this.a51.Location = new System.Drawing.Point(7, 124);
+            this.a51.Name = "a51";
+            this.a51.Size = new System.Drawing.Size(27, 20);
+            this.a51.TabIndex = 11;
+            // 
+            // a22
+            // 
+            this.a22.Location = new System.Drawing.Point(40, 46);
+            this.a22.Name = "a22";
+            this.a22.Size = new System.Drawing.Size(27, 20);
+            this.a22.TabIndex = 14;
+            // 
+            // a23
+            // 
+            this.a23.Location = new System.Drawing.Point(73, 46);
+            this.a23.Name = "a23";
+            this.a23.Size = new System.Drawing.Size(27, 20);
+            this.a23.TabIndex = 15;
+            // 
+            // a24
+            // 
+            this.a24.Location = new System.Drawing.Point(106, 46);
+            this.a24.Name = "a24";
+            this.a24.Size = new System.Drawing.Size(27, 20);
+            this.a24.TabIndex = 16;
+            // 
+            // a25
+            // 
+            this.a25.Location = new System.Drawing.Point(139, 46);
+            this.a25.Name = "a25";
+            this.a25.Size = new System.Drawing.Size(27, 20);
+            this.a25.TabIndex = 17;
+            // 
+            // a32
+            // 
+            this.a32.Location = new System.Drawing.Point(40, 72);
+            this.a32.Name = "a32";
+            this.a32.Size = new System.Drawing.Size(27, 20);
+            this.a32.TabIndex = 20;
+            // 
+            // a33
+            // 
+            this.a33.Location = new System.Drawing.Point(73, 72);
+            this.a33.Name = "a33";
+            this.a33.Size = new System.Drawing.Size(27, 20);
+            this.a33.TabIndex = 21;
+            // 
+            // a34
+            // 
+            this.a34.Location = new System.Drawing.Point(106, 72);
+            this.a34.Name = "a34";
+            this.a34.Size = new System.Drawing.Size(27, 20);
+            this.a34.TabIndex = 22;
+            // 
+            // a35
+            // 
+            this.a35.Location = new System.Drawing.Point(139, 72);
+            this.a35.Name = "a35";
+            this.a35.Size = new System.Drawing.Size(27, 20);
+            this.a35.TabIndex = 23;
+            // 
+            // a43
+            // 
+            this.a43.Location = new System.Drawing.Point(73, 98);
+            this.a43.Name = "a43";
+            this.a43.Size = new System.Drawing.Size(27, 20);
+            this.a43.TabIndex = 26;
+            // 
+            // a42
+            // 
+            this.a42.Location = new System.Drawing.Point(40, 98);
+            this.a42.Name = "a42";
+            this.a42.Size = new System.Drawing.Size(27, 20);
+            this.a42.TabIndex = 26;
+            // 
+            // a45
+            // 
+            this.a45.Location = new System.Drawing.Point(139, 98);
+            this.a45.Name = "a45";
+            this.a45.Size = new System.Drawing.Size(27, 20);
+            this.a45.TabIndex = 27;
+            // 
+            // a44
+            // 
+            this.a44.Location = new System.Drawing.Point(106, 98);
+            this.a44.Name = "a44";
+            this.a44.Size = new System.Drawing.Size(27, 20);
+            this.a44.TabIndex = 28;
+            // 
+            // a55
+            // 
+            this.a55.Location = new System.Drawing.Point(139, 124);
+            this.a55.Name = "a55";
+            this.a55.Size = new System.Drawing.Size(27, 20);
+            this.a55.TabIndex = 34;
+            // 
+            // a54
+            // 
+            this.a54.Location = new System.Drawing.Point(106, 124);
+            this.a54.Name = "a54";
+            this.a54.Size = new System.Drawing.Size(27, 20);
+            this.a54.TabIndex = 33;
+            // 
+            // a53
+            // 
+            this.a53.Location = new System.Drawing.Point(73, 124);
+            this.a53.Name = "a53";
+            this.a53.Size = new System.Drawing.Size(27, 20);
+            this.a53.TabIndex = 32;
+            // 
+            // a52
+            // 
+            this.a52.Location = new System.Drawing.Point(40, 124);
+            this.a52.Name = "a52";
+            this.a52.Size = new System.Drawing.Size(27, 20);
+            this.a52.TabIndex = 31;
+            // 
+            // doneMatrix
+            // 
+            this.doneMatrix.Controls.Add(this.dataGridView1);
+            this.doneMatrix.Location = new System.Drawing.Point(190, 13);
+            this.doneMatrix.Name = "doneMatrix";
+            this.doneMatrix.Size = new System.Drawing.Size(181, 150);
+            this.doneMatrix.TabIndex = 1;
+            this.doneMatrix.TabStop = false;
+            this.doneMatrix.Text = "Сформирована матрица";
+            this.doneMatrix.Enter += new System.EventHandler(this.doneMatrix_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Считать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(168, 124);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(268, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество чисел в матрице: \r\n ";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(135, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Рандом";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 254);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.doneMatrix);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Работа с двумерным массивом";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.doneMatrix.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox a55;
+        private System.Windows.Forms.TextBox a54;
+        private System.Windows.Forms.TextBox a53;
+        private System.Windows.Forms.TextBox a52;
+        private System.Windows.Forms.TextBox a44;
+        private System.Windows.Forms.TextBox a45;
+        private System.Windows.Forms.TextBox a42;
+        private System.Windows.Forms.TextBox a43;
+        private System.Windows.Forms.TextBox a35;
+        private System.Windows.Forms.TextBox a34;
+        private System.Windows.Forms.TextBox a33;
+        private System.Windows.Forms.TextBox a32;
+        private System.Windows.Forms.TextBox a25;
+        private System.Windows.Forms.TextBox a24;
+        private System.Windows.Forms.TextBox a23;
+        private System.Windows.Forms.TextBox a22;
+        private System.Windows.Forms.TextBox a51;
+        private System.Windows.Forms.TextBox a41;
+        private System.Windows.Forms.TextBox a31;
+        private System.Windows.Forms.TextBox a21;
+        private System.Windows.Forms.TextBox a15;
+        private System.Windows.Forms.TextBox a14;
+        private System.Windows.Forms.TextBox a13;
+        private System.Windows.Forms.TextBox a12;
+        private System.Windows.Forms.TextBox a11;
+        private System.Windows.Forms.GroupBox doneMatrix;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Two_Dimensional_Array_Sorter2
+{
+    public partial class Form1 : Form
+    {
+        int[,] matrix = new int[5, 5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            matrix[0, 0] = Convert.ToInt32(a11.Text);
+            matrix[0, 1] = Convert.ToInt32(a12.Text);
+            matrix[0, 2] = Convert.ToInt32(a13.Text);
+            matrix[0, 3] = Convert.ToInt32(a14.Text);
+            matrix[0, 4] = Convert.ToInt32(a15.Text);
+            matrix[1, 0] = Convert.ToInt32(a21.Text);
+            matrix[1, 1] = Convert.ToInt32(a22.Text);
+            matrix[1, 2] = Convert.ToInt32(a23.Text);
+            matrix[1, 3] = Convert.ToInt32(a24.Text);
+            matrix[1, 4] = Convert.ToInt32(a25.Text);
+            matrix[2, 0] = Convert.ToInt32(a31.Text);
+            matrix[2, 1] = Convert.ToInt32(a32.Text);
+            matrix[2, 2] = Convert.ToInt32(a33.Text);
+            matrix[2, 3] = Convert.ToInt32(a34.Text);
+            matrix[2, 4] = Convert.ToInt32(a35.Text);
+            matrix[3, 0] = Convert.ToInt32(a41.Text);
+            matrix[3, 1] = Convert.ToInt32(a42.Text);
+            matrix[3, 2] = Convert.ToInt32(a43.Text);
+            matrix[3, 3] = Convert.ToInt32(a44.Text);
+            matrix[3, 4] = Convert.ToInt32(a45.Text);
+            matrix[4, 0] = Convert.ToInt32(a51.Text);
+            matrix[4, 1] = Convert.ToInt32(a52.Text);
+            matrix[4, 2] = Convert.ToInt32(a53.Text);
+            matrix[4, 3] = Convert.ToInt32(a54.Text);
+            matrix[4, 4] = Convert.ToInt32(a55.Text);
+
+
+
+
+
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matrix[i,j].ToString();
+                }
+            }
+          
+            label1.Text += Convert.ToString(matrix.Length);
+        }
+     
+        private void doneMatrix_Enter(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+         
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = r.Next(-99, 99);
+
+                }
+            }
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matrix[i, j].ToString();
+                }
+            }
+           
+            label1.Text += Convert.ToString(matrix.Length);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] =0;
+
+                }
+            }
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                }
+            }
+          
+            label1.Text = "Количество чисел в массиве: ";
+        }
+    }
+}
